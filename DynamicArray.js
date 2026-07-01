aec36bd64a21db0d98e6fa77a2f2e53e824ddfc3
Razo-1 <razmikhak19@gmail.com>
2026-07-01 13:52:05 +0400

    added a field constructor also function which return size.

---
+++ DynamicArray.js
@@ -0,0 +1,23 @@
+class DynamicArray{
+    #arr;
+    #size;
+    #capacity;
+    #GROWTH;
+    #fill;
+
+    constructor(capacity = 8,fill = 0){
+        if(!Number.isInteger(capacity) || !Number.isInteger(fill)) throw new Error('Must be an integer number.')
+        if(capacity <= 0) throw new Error('Capacity must be an integer number')
+
+        this.#capacity = capacity;
+        this.#size = 0;
+        this.#GROWTH = 2;
+        this.#fill = fill;
+        this.#arr = new Int32Array(capacity);
+    }
+
+    size(){
+        return this.#size
+    }
+
+}
