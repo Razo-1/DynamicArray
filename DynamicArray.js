903a2cb90c3d111f934f40fc1eaf00764f56f064
Razo-1 <razmikhak19@gmail.com>
2026-07-01 14:57:55 +0400

    added insert  and  risize tools.

--- DynamicArray.js
+++ DynamicArray.js
@@ -39,4 +39,31 @@ class DynamicArray{
         if(!Number.isInteger(value)) throw new Error('Value Must be an integer number.');
         this.#arr[index] = value
     }
+    insert(index,value){
+        if(!Number.isInteger(index)) throw new Error('Index Must be an integer number.');
+        if(index < 0 || index >= this.#size) throw new Error('Index error');
+        if(!Number.isInteger(value)) throw new Error('Value Must be an integer number.');
+        if(this.#capacity === this.#size){
+            let newCap = this.#capacity * this.#GROWTH;
+            this.#resize(newCap);
+        }
+        for(let i = this.#size - 1;i >= index ;i++){
+            this.#arr[i] = this.#arr[i - 1];
+        }
+        this.#arr[index] = value;
+        ++this.#sizel;
+    }
+    #resize(newCap){
+        if(!Number.isInteger(newCap)) throw new Error('Must be an integer number.');
+        if(newCap <= 0) throw new Error('must be a positive number');
+        if(newCap < this.#size) throw new Error('must be larger than size');
+
+        let newArr = new Int32Array(newCap);
+
+        for(let i = 0;i < this.#size;i++){
+            newArr[i] = this.#arr[i];
+        }
+        this.#capacity = newCap;
+        this.#arr = newArr;
+    }
 }
